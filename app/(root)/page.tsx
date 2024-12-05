d9a049aa4339e017bc8847c5207cf38fd8333d39
michaldoman <mjd.domanski@gmail.com>
2024-12-05 11:21:56 +0100

    searchForm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import SearchForm from "@/app/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }:{
+  searchParams?: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query
+
   return(
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your start up, <br/>
+          Connect with entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote for Pitches, and get noticed in Virtual Competitions
+        </p>
+
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
