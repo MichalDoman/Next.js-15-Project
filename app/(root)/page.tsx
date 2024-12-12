367c07be1457f74c7fc98ba32aad4622f3be5ba2
michaldoman <mjd.domanski@gmail.com>
2024-12-12 15:08:01 +0100

    sanity live

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupCardType} from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }:{
   searchParams?: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY});
   console.log(`POSTS: ${posts}`)
 
   return(
@@ -41,6 +41,8 @@ export default async function Home({ searchParams }:{
         </ul>
 
       </section>
+
+      <SanityLive />
     </>
   );
 }
